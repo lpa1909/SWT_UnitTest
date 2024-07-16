0aa1897b1cf7f37ef1b07757199d6b5aeebdf69c
kimquy <LanNTHE172785@fpt.edu.vn>
2024-07-16 16:23:33 +0700

    upload manage add product

---
+++ src/java/controller/signUpServlet.java
@@ -0,0 +1,149 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller;
+
+import dal.DBContext;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+import model.Account;
+
+/**
+ *
+ * @author NGUYEN LAN
+ */
+@WebServlet(name="signUpServlet", urlPatterns={"/signup"})
+public class signUpServlet extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet signUpServlet</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet signUpServlet at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//    throws ServletException, IOException {
+//        
+//        try {
+//            String user = request.getParameter("user");
+//            String email = request.getParameter("email");
+//            String pass = request.getParameter("pass");
+//            
+//            DBContext db = new DBContext();
+//            Account a = db.checkEmail(email);
+//            if(a==null){ // a== null nghĩa là chưa có email đăng kí kia, được signup
+//                db.signUp(user, email, pass);
+//                response.sendRedirect("home");
+//            }else{
+//                request.setAttribute("mess2", "Your account is exist already!");
+//                request.getRequestDispatcher("signup.jsp").forward(request, response);
+//            }
+//        } catch (SQLException ex) {
+//            Logger.getLogger(signUpServlet.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        
+//        
+//    }
+        protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        
+        try {
+            String user = request.getParameter("user");
+            String email = request.getParameter("email");
+            String pass = request.getParameter("pass");
+            
+            DBContext db = new DBContext();
+            if (!isValidName(user) || !isValidEmail(email) || !isValidPassword(pass)) {
+            request.setAttribute("mess2", "Your input is invalid!");
+            request.getRequestDispatcher("signup.jsp").forward(request, response);
+            return;
+        }
+            Account a = db.checkEmail(email);
+            if(a==null){ // a== null nghĩa là chưa có email đăng kí kia, được signup
+                db.signUp(user, email, pass);
+                response.sendRedirect("home");
+            }else{
+                request.setAttribute("mess2", "Your account is exist already!");
+                request.getRequestDispatcher("signup.jsp").forward(request, response);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(signUpServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+    }
+
+    public boolean isValidName(String name) {
+        return Pattern.matches("^[a-zA-Z\\s]{2,10}$", name);
+    }
+
+    public boolean isValidEmail(String email) {
+        // Biểu thức chính quy kiểm tra email (đơn giản)
+        return Pattern.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$", email);
+    }
+
+    public boolean isValidPassword(String password) {
+        // Biểu thức chính quy kiểm tra mật khẩu (ít nhất 1 chữ hoa, 1 chữ thường, 1 số và 1 ký tự đặc biệt)
+        return Pattern.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).*$", password); 
+        }
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ test/Test/LogoutTest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package Test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author NGUYEN LAN
+ */
+public class LogoutTest {
+    
+    public void LogoutTest() {
+        
+    }
+    
+}
